25e2973435251cdbd1bc8d1ac7e2ae3a7a98090b
doaaaliahmed <doaaali3@yahoo.com>
2022-11-21 21:29:53 +0200

    getting data from firebase

--- src/Components/Menu/MenuList.js
+++ src/Components/Menu/MenuList.js
@@ -1,43 +1,68 @@
-
+import {useState, useEffect} from 'react';
 import MealItem from "./MealItem";
 
-const list = [
-  {
-    id: "l1",
-    name: "Sushi",
-    description: "Finest fish and vegetables",
-    price: 22.99,
-  },
-  {
-    id: "l2",
-    name: "Schnitzel",
-    description: "A german specialty !",
-    price: 16.5,
-  },
-  {
-    id: "l3",
-    name: "Barbecue Burger",
-    description: "American raw , meaty",
-    price: 12.99,
-  },
-  {
-    id: "l4",
-    name: "German Bowl",
-    description: "Healty... and green...",
-    price: 18.99,
-  },
-];
+
+const Loading = ()=>{
+  return <p style={{textAlign:'center', fontSize:'1.5rem'}}>Loading ...</p>
+}
 
 const MenuList = (props) => {
-  
-  
+  const [isLoading , setIsLoading] = useState(true);
+  const [hasError , setHasError] = useState(null);
+  const [list,setList] = useState([]);
+
+  useEffect(()=>{
+
+      const mealListApi = async function(){
+        const response = await fetch('https://food-order-app-e3eaa-default-rtdb.firebaseio.com/meals.json');
+       
+        if(!response.ok){
+          
+          throw new Error('something went wrong');
+        } 
+       
+        const data = await  response.json(); 
+        const loadedList = [];
+          for(let key in data){
+            
+            loadedList.push({
+              id: key,
+              name : data[key].name,
+              description: data[key].description,
+              price: data[key].price
+            })
+        
+          }
+        setList(loadedList)
+        setIsLoading(false);
+        
+          
+         }
+
+       
+          mealListApi().catch(error => {
+          setIsLoading(false)
+          setHasError(error.message);
+        })
+          
+        
+         
+  },[])
+
 
   return (
+  <>
+  { isLoading ? <Loading/> : 
     <ul>
       {list.map((l) => (
         <MealItem id={l.id} key={l.id} name={l.name} description={l.description} price={l.price}/>
-      ))}
+      ))}  
+      {hasError && <p>somthing went wrong {hasError}</p>}
     </ul>
+}
+    </>
+    
+ 
   );
 };
 
